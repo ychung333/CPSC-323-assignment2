3d5cd99ca70550421abd003c770328a819e2fd0f
Amank1243 <148727967+Amank1243@users.noreply.github.com>
2026-04-03 17:35:22 -0700

    Added creation of output file, added test cases
    Reused code from Assignment 1 to create an output file if one isn't provided. I then ran the program to test it against case1.
    The program is broken and needs fixing, but looked okay at the start.

--- code/main.cpp
+++ code/main.cpp
@@ -9,22 +9,33 @@ using namespace std;
 int main()
 {
     string inputFileName;
-    string outputFileName;
+    // string outputFileName;
 
     cout << "Enter input file name: ";
     cin >> inputFileName;
 
-    cout << "Enter output file name: ";
-    cin >> outputFileName;
+    // cout << "Enter output file name: ";
+    // cin >> outputFileName;
 
     ifstream fin(inputFileName);
-    ofstream fout(outputFileName);
+    // ofstream fout(outputFileName);
 
     if (!fin)
     {
         cout << "Error: could not open input file." << endl;
         return 1;
     }
+
+     // make output file and write to it
+    string base = inputFileName;
+    size_t pos = base.find_last_of("/\\");
+    if (pos != string::npos)
+    {
+        base = base.substr(pos + 1);
+    }
+
+    string outputFileName = "Output_" + base;
+    ofstream fout(outputFileName);
 
     if (!fout)
     {
